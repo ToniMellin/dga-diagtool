f0260d25e317e81e79b7a9f3b035dc253f4a6e6e
Toni Mellin <55407190+ToniMellin@users.noreply.github.com>
2022-07-11 16:03:15 +0300

    triangle 1 tests added based on excel

--- tests/test_diagnostic_calculation.py
+++ tests/test_diagnostic_calculation.py
@@ -1,8 +1,8 @@
+import numpy as np
 from diagnostic import diagnostic_calculation
 from diagnostic import duval_triangle_1
 from diagnostic import duval_triangle_4
 from diagnostic import duval_triangle_5
-import numpy as np
 
 # ratio tests
 # TODO add more ratios
@@ -21,23 +21,44 @@ def test_iec_ratio_nan():
 def test_duval_1_result_calculation_DT_T3_edge_1():
     assert duval_triangle_1.calculate_duval_1_result(35, 15, 50) == 'T3'
 
+def test_duval_1_result_calculation_DT_T2_T3_cross():
+    assert duval_triangle_1.calculate_duval_1_result(46, 4, 50) == 'T3'
+
 def test_duval_1_result_calculation_T2_T3_edge_1():
     assert duval_triangle_1.calculate_duval_1_result(48, 2, 50) == 'T3'
 
-def test_duval_1_result_calculation_DT_T2_T3_cross():
-    assert duval_triangle_1.calculate_duval_1_result(46, 4, 50) == 'T3'
+def test_duval_1_result_calculation_T2_DT_between():
+    assert duval_triangle_1.calculate_duval_1_result(61, 4, 35) == 'T3'
 
 def test_duval_1_result_calculation_DT_T1_T2_cross():
     assert duval_triangle_1.calculate_duval_1_result(76, 4, 20) == 'T1'
 
-def test_duval_1_result_calculation_D1_D2_DT_cross():
-    assert duval_triangle_1.calculate_duval_1_result(63, 31, 24) == 'D2'
+def test_duval_1_result_calculation_T1_T2_edge():
+    assert duval_triangle_1.calculate_duval_1_result(78, 2, 20) == 'T2'
 
-def test_duval_1_result_calculation_D1_D2_edge():
-    assert duval_triangle_1.calculate_duval_1_result(26, 50, 24) == 'D2'
+def test_duval_1_result_calculation_T1_DT_between():
+    assert duval_triangle_1.calculate_duval_1_result(86, 4, 10) == 'T1'
 
-def test_duval_1_result_calculation_PD_T1_edge():
+def test_duval_1_result_calculation_PD_T1_right_edge():
+    assert duval_triangle_1.calculate_duval_1_result(98, 0, 2) == 'PD'
+
+def test_duval_1_result_calculation_PD_T1_left_edge():
     assert duval_triangle_1.calculate_duval_1_result(98, 2, 0) == 'PD'
+
+def test_duval_1_result_calculation_PD_T1_DT_cross():
+    assert duval_triangle_1.calculate_duval_1_result(96, 4, 0) == 'T1'
+
+def test_duval_1_result_calculation_D1_DT_edge():
+    assert duval_triangle_1.calculate_duval_1_result(87, 13, 0) == 'D1'
+
+def test_duval_1_result_calculation_D1_DT_between():
+    assert duval_triangle_1.calculate_duval_1_result(77, 13, 10) == 'D1'
+
+def test_duval_1_result_calculation_D1_D2_DT_cross():
+    assert duval_triangle_1.calculate_duval_1_result(64, 13, 23) == 'D1'
+
+def test_duval_1_result_calculation_D2_DT_between():
+    assert duval_triangle_1.calculate_duval_1_result(57, 13, 30) == 'D2'
 
 def test_duval_1_result_calculation_D2_DT_edge():
     assert duval_triangle_1.calculate_duval_1_result(47, 13, 40) == 'D2'
@@ -45,13 +66,36 @@ def test_duval_1_result_calculation_D2_DT_edge():
 def test_duval_1_result_calculation_D2_DT_edge_2():
     assert duval_triangle_1.calculate_duval_1_result(31, 29, 40) == 'D2'
 
+def test_duval_1_result_calculation_D2_DT_edge_3():
+    assert duval_triangle_1.calculate_duval_1_result(15, 29, 56) == 'D2'
+
+def test_duval_1_result_calculation_D2_DT_edge_4():
+    assert duval_triangle_1.calculate_duval_1_result(0, 29, 71) == 'D2'
+
 def test_duval_1_result_calculation_D1_D2_edge():
-    assert duval_triangle_1.calculate_duval_1_result(64, 13, 23) == 'D1'
+    assert duval_triangle_1.calculate_duval_1_result(37, 40, 23) == 'D1'
+
+def test_duval_1_result_calculation_D1_D2_edge_2():
+    assert duval_triangle_1.calculate_duval_1_result(0, 77, 23) == 'D1'
+
+def test_duval_1_result_calculation_DT_T3_edge_2():
+    assert duval_triangle_1.calculate_duval_1_result(0, 15, 85) == 'T3'
+
+def test_duval_1_result_calculation_DT_T3_edge_2():
+    assert duval_triangle_1.calculate_duval_1_result(20, 15, 65) == 'T3'
+
+def test_duval_1_result_calculation_DT_T3_edge_2():
+    assert duval_triangle_1.calculate_duval_1_result(40, 10, 50) == 'T3'
 
 # Duval triangle 4 tests
 # TODO add tests for all cases and edge events
 def test_duval_4_result_calculation_C_edge_1():
     assert duval_triangle_4.calculate_duval_4_result(10, 26, 64) == 'C'
+
+def test_duval_4_result_calculation_C_edge_2():
+    assert duval_triangle_4.calculate_duval_4_result(40, 24, 36) == 'C'
+
+
 
 # Duval triangle 5 restes
 # TODO add tests for all cases and edge events
