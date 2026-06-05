0d76cd0f0a38c31ecfa45c5f8472d3c6e667eb84
Toni Mellin <55407190+ToniMellin@users.noreply.github.com>
2026-06-05 11:16:33 +0300

    axis details formatting option to duval triangle 2

--- diagnostic/duval_triangle_2.py
+++ diagnostic/duval_triangle_2.py
@@ -756,8 +756,31 @@ def create_duval_2_group_density_distribution_graph(ch4_groups, c2h2_groups, c2h
     else:
         marker_compare = False
 
+    if 'show_axis_details' in kwargs:
+        show_axis_details = kwargs['show_axis_details']
+    else:
+        show_axis_details = False
+
 
     fig = ternary_density_distribution_graph(ch4_groups, c2h2_groups, c2h4_groups, axis_names=['CH4', 'C2H2', 'C2H4'], color_scale=color_scale, showscale=show_scale, cutoff=cutoff, cutoff_direction=cutoff_direction, discard_zeros=discard_zeros, show_markers=show_markers, contour_n=contour_n, group_names=group_names, marker_compare=marker_compare)
+
+    if show_axis_details == True:
+        fig.update_ternaries(
+                    aaxis_title=dict(text=''), 
+                    aaxis=dict(linecolor='#000000', showgrid=True, gridcolor='lightgrey', tickmode='linear', dtick=10, tick0=0, ticklabelstep=2, griddash='solid', ticksuffix=' %', hoverformat='CH4=%%{a}<br>C2H2=%%{b}<br>C2H4=%%{b}<extra></extra>'), 
+                    baxis_title=dict(text=''),
+                    baxis=dict(linecolor='#000000', showgrid=True, gridcolor='lightgrey', tickmode='linear', dtick=10, tick0=0, ticklabelstep=2, griddash='solid', ticksuffix=' %', hoverformat='CH4=%%{a}<br>C2H2=%%{b}<br>C2H4=%%{b}<extra></extra>'),
+                    caxis_title=dict(text=''), 
+                    caxis=dict(linecolor='#000000', showgrid=True, gridcolor='lightgrey', tickmode='linear', dtick=10, tick0=0, ticklabelstep=2, griddash='solid', ticksuffix=' %', hoverformat='CH4=%%{a}<br>C2H2=%%{b}<br>C2H4=%%{b}<extra></extra>'))
+        fig.add_annotation(x=0.3, y=0.6, xref='paper', yref='paper',
+            text="CH4", textangle=300,
+            showarrow=True, arrowhead=1, ax=-15, ay=30, axref='x', ayref='y')
+        fig.add_annotation(x=0.7, y=0.5, xref='paper', yref='paper',
+            text="C2H4", textangle=60,
+            showarrow=True, arrowhead=1, ax=-15, ay=-30, axref='x', ayref='y')
+        fig.add_annotation(x=0.4, y=-0.07, xref='paper', yref='paper',
+            text="C2H2", textangle=0,
+            showarrow=True, arrowhead=1, ax=35, ay=0, axref='x', ayref='y')
 
     if len(ch4_groups) > 1:
         for g in range(1, len(ch4_groups)+1):
